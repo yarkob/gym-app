b98cd47c86d546af31f3334b1fb1c6f69580da1e
Vitalii <vitalii.bondarenko.ua@gmail.com>
2022-07-31 11:03:38 +0300

    Create Home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+import ExerciseDetail from './pages/Exercise';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-   <div>Gym app</div>
-  );
+    <Box width="400px" sx={{ width: { xl: '1488px'}}} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  )
 }
 
 export default App;
